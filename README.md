12b07acc1b7cef1817d6b5a7bdccbfd893cf2642
saperduper <bobsap@gmail.com>
2011-11-13 19:34:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Code for http://gsis-wsnp.appspot.com, a simple client for the SOAP WS provided by GSIS (http://www.gsis.gr/wsnp.html)
+
+---
+Contains:
+* [webapp2](http://webapp-improved.appspot.com)
+* [suds](https://fedorahosted.org/suds) (slightly modified)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,67 @@
+from google.appengine.ext.webapp import template
+from suds.client import Client
+from webapp2 import RequestHandler, WSGIApplication
+from xml.etree import cElementTree
+from xml2Dict.xml2Dict import XmlDictConfig
+import os
+
+WSDL_URL = 'http://www1.gsis.gr/wsgsis/RgWsBasStoixN/RgWsBasStoixNSoapHttpPort?wsdl'
+NULL = "{'{http://www.w3.org/2001/XMLSchema-instance}nil': '1'}"
+RgWsBasStoixNRtUser_TYPE = 'ns0:RgWsBasStoixNRtUser'
+GenWsErrorRtUser_TYPE = 'ns0:GenWsErrorRtUser'
+
+class AfmHandler(RequestHandler):
+    def get(self, afm):
+        client = Client(url = WSDL_URL, location = WSDL_URL, retxml = True)
+        # prepare the request
+        pBasStoixNRec_out = client.factory.create(RgWsBasStoixNRtUser_TYPE)
+        pBasStoixNRec_out.afm = ' '
+        pErrorRec_out = client.factory.create(GenWsErrorRtUser_TYPE)
+        pErrorRec_out.errorDescr = ' '
+        pErrorRec_out.errorCode = ' '
+        # call the service
+        reply = client.service.rgWsBasStoixN(pAfm = afm,
+                                            pBasStoixNRec_out = pBasStoixNRec_out,
+                                            pCallSeqId_out = 0,
+                                            pErrorRec_out = pErrorRec_out)
+        # reply as xml
+        xml = cElementTree.XML(reply)
+        # xml -> dict
+        dictionary = XmlDictConfig(xml)
+        # check for errors
+        error = dictionary.get('Body').get('rgWsBasStoixNResponse').get('pErrorRec_out')
+        error_desc = error.get('errorDescr')
+        # in case of no errors, error_desc is dict
+        if type(error_desc) is unicode:
+            template_values = {
+                'error_message': error_desc
+            }
+            path = os.path.join(os.path.dirname(__file__), 'templates/error.html')
+            self.response.out.write(template.render(path, template_values))
+        else:
+            # fetch the details
+            details = dictionary.get('Body').get('rgWsBasStoixNResponse').get('pBasStoixNRec_out')
+            template_values = {
+                'name': details.get('onomasia'),
+                'address': details.get('postalAddress'),
+                'city': details.get('parDescription'),
+                'number': details.get('postalAddressNo'),
+                'tk': details.get('postalZipCode'),
+                'telephone': details.get('firmPhone'),
+                'fax': details.get('firmFax'),
+                'afm': details.get('afm'),
+                'doy': details.get('doy'),
+                'doyDescr': details.get('doyDescr')
+            }
+            path = os.path.join(os.path.dirname(__file__), 'templates/result.html')
+            self.response.out.write(template.render(path, template_values))
+
+app = WSGIApplication([
+    (r'/(\d{9})', AfmHandler),
+])
+
+def main():
+    app.run()
+
+if __name__ == '__main__':
+    main()
